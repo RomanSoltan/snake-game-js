0f859f34595b4ea7b8955f3ef6300190c15411d5
Roman Soltan <romabandit88@gmail.com>
2025-12-06 17:59:43 +0200

    fix bug direction

--- src/main.js
+++ src/main.js
@@ -122,16 +122,16 @@ function handleKeyPress(event) {
   } else {
     switch (event.key) {
       case "ArrowUp":
-        direction = "up";
+        if (direction !== "down") direction = "up";
         break;
       case "ArrowDown":
-        direction = "down";
+        if (direction !== "up") direction = "down";
         break;
       case "ArrowLeft":
-        direction = "left";
+        if (direction !== "right") direction = "left";
         break;
       case "ArrowRight":
-        direction = "right";
+        if (direction !== "left") direction = "right";
         break;
     }
   }
